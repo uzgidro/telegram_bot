6b7a456fa50f3f7670dac411e1aca4601d8862c4
suxa56 <suxa5600@gmail.com>
2024-09-26 16:24:43 +0400

    paging

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\service\HttpService;
 use App\Models\CallbackData;
 use App\Models\MessageType;
 use App\Models\UpdateTG;
@@ -11,6 +12,17 @@ use Illuminate\Support\Facades\Http;
 
 class AdminController
 {
+    private HttpService $httpService;
+
+    /**
+     * @param HttpService $httpService
+     */
+    public function __construct(HttpService $httpService)
+    {
+        $this->httpService = $httpService;
+    }
+
+
     /**
      * @param Users $user
      * @param UpdateTG|null $update
@@ -18,14 +30,18 @@ class AdminController
      */
     public function anticor(Users $user, ?UpdateTG $update): void
     {
+        $callbackData = $update->callbackQuery->data;
         $limit = 5;
-        if ($update->callbackQuery->data == CallbackData::INCOME_ANTICOR) {
+        if ($callbackData == CallbackData::INCOME_ANTICOR) {
             $collection = DB::table('messages')->where('type', MessageType::ANTICOR)->limit($limit)->get();
             $count = DB::table('messages')->where('type', MessageType::ANTICOR)->count();
             $text = '';
             foreach ($collection as $item) {
                 $formattedDate = date('H:i d.m.Y', strtotime($item->created_at));
-                $text .= "*ID: " . $item->id . "*\n" . $item->text . "\n".$formattedDate."\n\n\n";
+                $text .= "*ID: " . $item->id . "*\n" . $item->text . "\n" . $formattedDate . "\n\n\n";
+            }
+            if (isset($update->callbackQuery->message->chat->id)) {
+                $this->httpService->reactToCallback($update);
             }
             Http::post('https://api.telegram.org/bot7849210506:AAHwUp5nF6nWxxfEoEH8NVBP6CwyRtHUx7s/sendMessage', [
                 'chat_id' => $user->chat_id,
@@ -36,13 +52,22 @@ class AdminController
                         $count > $limit ? [
 //                            ['text' => '⏪', 'callback_data' => CallbackData::ANTICOR_FIRST],
 //                            ['text' => '◀️', 'callback_data' => CallbackData::ANTICOR_PREVIOUS],
-                            ['text' => '1/'.ceil($count/$limit), 'callback_data' => CallbackData::BLANK],
-                            ['text' => '▶️', 'callback_data' => CallbackData::ANTICOR_NEXT],
-                            ['text' => '⏩', 'callback_data' => CallbackData::ANTICOR_LAST],
+                            ['text' => '1/' . ceil($count / $limit), 'callback_data' => CallbackData::BLANK],
+                            ['text' => '▶️', 'callback_data' => CallbackData::ANTICOR_PAGING_PREFIX . '2'],
+                            ['text' => '⏩', 'callback_data' => CallbackData::ANTICOR_PAGING_LAST],
                         ] : [],
                     ]
                 ]),
             ]);
+        } elseif (str_starts_with($callbackData, CallbackData::ANTICOR_PAGING_PREFIX)) {
+            if ($callbackData == CallbackData::ANTICOR_PAGING_FIRST) {
+                $collection = DB::table('messages')->where('type', MessageType::ANTICOR)->limit($limit)->get();
+            } elseif ($callbackData == CallbackData::ANTICOR_PAGING_LAST) {
+
+            } else {
+                $offset = (intval(str_replace(CallbackData::ANTICOR_PAGING_PREFIX, '', $callbackData)) - 1) * 5;
+                $collection = DB::table('messages')->where('type', MessageType::ANTICOR)->limit($limit)->offset($offset)->get();
+            }
         }
     }
 }
--- app/Models/CallbackData.php
+++ app/Models/CallbackData.php
@@ -12,10 +12,9 @@ class CallbackData
     public const INCOME_ANTICOR = 'INCOME_ANTICOR';
 
     // Paging
-    public const ANTICOR_FIRST = 'ANTICOR_FIRST';
-    public const ANTICOR_PREVIOUS = 'ANTICOR_PREVIOUS';
-    public const ANTICOR_NEXT = 'ANTICOR_NEXT';
-    public const ANTICOR_LAST = 'ANTICOR_LAST';
+    public const ANTICOR_PAGING_PREFIX = 'ANTICOR_PAGING';
+    public const ANTICOR_PAGING_FIRST = 'ANTICOR_PAGING_FIRST';
+    public const ANTICOR_PAGING_LAST = 'ANTICOR_PAGING_LAST';
 
     // Languages
     public const LANGUAGE_RU = 'LANGUAGE_RU';
